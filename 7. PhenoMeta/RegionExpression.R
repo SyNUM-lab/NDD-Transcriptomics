d849e00f386e572f42e15eb6a170a09a23610ffe
Jarno Koetsier <jarno.koetsier@gmail.com>
2025-05-26 14:46:10 +0200

    Update RegionExpression.R

--- 7. PhenoMeta/RegionExpression.R
+++ 7. PhenoMeta/RegionExpression.R
@@ -21,6 +21,222 @@ library(tidyverse)
 library(Seurat)
 library(sparseMatrixStats)
 library(patchwork)
+library(zellkonverter)
+library(DelayedArray)
+library(SummarizedExperiment)
+
+################################################################################
+
+# scRNA-seq from first trimester developing human  brain
+# https://cellxgene.cziscience.com/collections/4d8fed08-2d6d-4692-b5ea-464f1d072077
+
+################################################################################
+
+# Set data directory
+dataDir <- "Data/EarlyDevelopment/"
+
+# Load data
+sce <- readH5AD(paste0(dataDir, "f5960e39-50cd-4b31-af54-e70e1b5517a8.h5ad"), 
+                use_hdf5 = TRUE)
+
+# Extract LHX1 expression
+lhx1 <- assay(sce, "X")["ENSG00000273706", ]
+
+# Extract LHX5 expression
+lhx5 <- assay(sce, "X")["ENSG00000089116", ]
+
+# Get annotations/metadata
+metaData <- as.data.frame(colData(sce))
+all(names(lhx1) == rownames(metaData))
+
+# Add LHX1/5 expression
+metaData$LHX1 <- lhx1
+metaData$LHX5 <- lhx5
+
+# Save metadata
+save(metaData, file = paste0(dataDir, "metaData.RData"))
+
+# Load metadata
+load(paste0(dataDir, "metaData.RData"))
+
+#==============================================================================#
+# All cells combined
+#==============================================================================#
+
+# make copy of metadata
+metaData_fil <- metaData
+
+# Prepare metadata for plotting
+plotDF <- unique(metaData_fil[,c("Region", "Age")])
+plotDF$LHX1_mean <- NA
+plotDF$LHX1_n <- NA
+plotDF$LHX5_mean <- NA
+plotDF$LHX5_n <- NA
+for (i in 1:nrow(plotDF)){
+  LHX1expr <- (metaData_fil$LHX1 != 0) & (metaData_fil$Region == plotDF$Region[i]) & (metaData_fil$Age == plotDF$Age[i])
+  plotDF$LHX1_mean[i] <- mean(metaData_fil$LHX1[which(LHX1expr)])
+  plotDF$LHX1_n[i] <- sum(LHX1expr)/length(LHX1expr)
+  
+  
+  LHX5expr <- (metaData_fil$LHX5 != 0) & (metaData_fil$Region == plotDF$Region[i]) & (metaData_fil$Age == plotDF$Age[i])
+  plotDF$LHX5_mean[i] <- mean(metaData_fil$LHX5[which(LHX5expr)])
+  plotDF$LHX5_n[i] <- sum(LHX5expr)/length(LHX5expr)
+}
+
+plotDF$Age_num <- as.numeric(as.character(plotDF$Age))
+
+
+# Exclude head and brain samples 
+plotDF <- plotDF[!(plotDF$Region %in% c("Head", "Brain")),]
+
+# Set order of brain regions
+plotDF$Region <- factor(plotDF$Region,
+                        levels = c("Hindbrain", "Medulla", "Cerebellum", "Pons",
+                                   "Midbrain",
+                                   "Forebrain","Diencephalon", "Telencephalon"))
+
+# Set colors
+color <- setNames(c(rev(c("#FEE0D2", "#FC9272","#EF3B2C","#A50F15")),
+                    "#74C476",
+                    rev(c("#C6DBEF", "#6BAED6","#08519C"))),
+                  c("Hindbrain", "Medulla", "Cerebellum", "Pons",
+                    "Midbrain",
+                    "Forebrain","Diencephalon", "Telencephalon"))
+
+# Set shapes
+shape <- setNames(c(rep(16,4),
+                    15,
+                    rep(17,3)),
+                  c("Hindbrain", "Medulla", "Cerebellum", "Pons",
+                    "Midbrain",
+                    "Forebrain","Diencephalon", "Telencephalon"))
+
+# Make plot for LHX1
+p <- ggplot(plotDF) +
+  geom_point(aes(x = Age_num, y = LHX1_mean, color = Region, size = LHX1_n,
+                 shape = Region)) +
+  labs(color = "Region",
+       size = "Cell\nproportion",
+       y = "Mean expression",
+       x = "Age (pcw)") +
+  scale_color_manual(values = color) +
+  scale_shape_manual(values = shape) +
+  guides(color = guide_legend(order=1),
+         shape = guide_legend(order=1),
+         size = guide_legend(order=2)) +
+  theme_bw()
+
+# Save plot
+ggsave(p, file = "7. PhenoMeta/EarlyDevelopment/LHX1_all.png", width = 7, height = 5)
+
+
+# Make plot for LHX5
+p <- ggplot(plotDF) +
+  geom_point(aes(x = Age_num, y = LHX5_mean, color = Region, size = LHX5_n,
+                 shape = Region)) +
+  labs(color = "Region",
+       size = "Cell\nproportion",
+       y = "Mean expression",
+       x = "Age (pcw)") +
+  scale_color_manual(values = color) +
+  scale_shape_manual(values = shape) +
+  guides(color = guide_legend(order=1),
+         shape = guide_legend(order=1),
+         size = guide_legend(order=2)) +
+  theme_bw()
+
+# Save plot
+ggsave(p, file = "7. PhenoMeta/EarlyDevelopment/LHX5_all.png", width = 7, height = 5)
+
+
+#==============================================================================#
+# Neuronal cell fraction only
+#==============================================================================#
+
+# filter metadata
+metaData_fil <- metaData[metaData$cell_type == "neuron",]
+
+# Prepare metadata for plotting
+plotDF <- unique(metaData_fil[,c("Region", "Age")])
+plotDF$LHX1_mean <- NA
+plotDF$LHX1_n <- NA
+plotDF$LHX5_mean <- NA
+plotDF$LHX5_n <- NA
+for (i in 1:nrow(plotDF)){
+  LHX1expr <- (metaData_fil$LHX1 != 0) & (metaData_fil$Region == plotDF$Region[i]) & (metaData_fil$Age == plotDF$Age[i])
+  plotDF$LHX1_mean[i] <- mean(metaData_fil$LHX1[which(LHX1expr)])
+  plotDF$LHX1_n[i] <- sum(LHX1expr)/length(LHX1expr)
+  
+  
+  LHX5expr <- (metaData_fil$LHX5 != 0) & (metaData_fil$Region == plotDF$Region[i]) & (metaData_fil$Age == plotDF$Age[i])
+  plotDF$LHX5_mean[i] <- mean(metaData_fil$LHX5[which(LHX5expr)])
+  plotDF$LHX5_n[i] <- sum(LHX5expr)/length(LHX5expr)
+}
+
+plotDF$Age_num <- as.numeric(as.character(plotDF$Age))
+
+
+# Exclude head and brain samples 
+plotDF <- plotDF[!(plotDF$Region %in% c("Head", "Brain")),]
+
+# Set order of brain regions
+plotDF$Region <- factor(plotDF$Region,
+                        levels = c("Hindbrain", "Medulla", "Cerebellum", "Pons",
+                                   "Midbrain",
+                                   "Forebrain","Diencephalon", "Telencephalon"))
+
+# Set colors
+color <- setNames(c(rev(c("#FEE0D2", "#FC9272","#EF3B2C","#A50F15")),
+                    "#74C476",
+                    rev(c("#C6DBEF", "#6BAED6","#08519C"))),
+                  c("Hindbrain", "Medulla", "Cerebellum", "Pons",
+                    "Midbrain",
+                    "Forebrain","Diencephalon", "Telencephalon"))
+
+# Set shapes
+shape <- setNames(c(rep(16,4),
+                    15,
+                    rep(17,3)),
+                  c("Hindbrain", "Medulla", "Cerebellum", "Pons",
+                    "Midbrain",
+                    "Forebrain","Diencephalon", "Telencephalon"))
+
+# Make plot for LHX1
+p <- ggplot(plotDF) +
+  geom_point(aes(x = Age_num, y = LHX1_mean, color = Region, size = LHX1_n,
+                 shape = Region)) +
+  labs(color = "Region",
+       size = "Cell\nproportion",
+       y = "Mean expression",
+       x = "Age (pcw)") +
+  scale_color_manual(values = color) +
+  scale_shape_manual(values = shape) +
+  guides(color = guide_legend(order=1),
+         shape = guide_legend(order=1),
+         size = guide_legend(order=2)) +
+  theme_bw()
+
+# Save plot
+ggsave(p, file = "7. PhenoMeta/EarlyDevelopment/LHX1_neuronal.png", width = 7, height = 5)
+
+
+# Make plot for LHX5
+p <- ggplot(plotDF) +
+  geom_point(aes(x = Age_num, y = LHX5_mean, color = Region, size = LHX5_n,
+                 shape = Region)) +
+  labs(color = "Region",
+       size = "Cell\nproportion",
+       y = "Mean expression",
+       x = "Age (pcw)") +
+  scale_color_manual(values = color) +
+  scale_shape_manual(values = shape) +
+  guides(color = guide_legend(order=1),
+         shape = guide_legend(order=1),
+         size = guide_legend(order=2)) +
+  theme_bw()
+
+# Save plot
+ggsave(p, file = "7. PhenoMeta/EarlyDevelopment/LHX5_neuronal.png", width = 7, height = 5)
 
 
 ################################################################################
